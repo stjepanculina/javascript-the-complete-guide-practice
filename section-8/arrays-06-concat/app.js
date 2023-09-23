75edb3b0777b3cee8789f8bf7b88a058569e8860
Stipe <stip1culin@gmail.com>
2023-09-23 18:01:14 +0200

    8. 190. concat()

---
+++ section-8/arrays-06-concat/app.js
@@ -0,0 +1,52 @@
+// const numbers = [1, 2, 3];
+// console.log(numbers);
+
+// // const moreNumbers = Array(5, 2);
+// // console.log(moreNumbers);
+
+// // const yetMoreNumbers = Array.of(1, 2);
+// // console.log(yetMoreNumbers);
+
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems);
+
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems);
+
+// const hobbies = ['Cooking', 'Sports'];
+// const personalData = [30, 'Max', {moreDetail: []}];
+
+// const analyticsData = [[1, 1.6], [-5.4, 2.1]];
+
+// for (const data of analyticsData) {
+//   for (const dataPoint of data) {
+//     console.log(dataPoint);
+//   }
+// }
+
+// console.log(personalData[1]);
+
+// const hobbies = ['Sports', 'Cooking'];
+// hobbies.push('Reading');
+// hobbies.unshift('Coding');
+// const poppedValue = hobbies.pop();
+// hobbies.shift();
+// console.log(hobbies);
+
+// hobbies[1] = 'Coding';
+// // hobbies[5] = 'Reading'; // rarely used
+// console.log(hobbies, hobbies[4]);
+
+// hobbies.splice(1, 0, 'Good Food');
+// console.log(hobbies);
+
+// const removedElements = hobbies.splice(-2, 1);
+// console.log(hobbies);
+
+const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+// const storedResults = testResults.slice(2);
+const storedResults = testResults.concat( [3.99, 2] ); // unlike push(), concat() will add new element/s to the existing array and will also return a brand new array with parameters if added
+
+testResults.push(5.91);
+
+console.log(storedResults, testResults);
