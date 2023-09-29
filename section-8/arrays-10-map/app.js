de02bfd31a799e7fa4990e376fd3df278bb3ff2f
stjepanculina <145278455+stjepanculina@users.noreply.github.com>
2023-09-29 21:30:11 +0200

    Update on comment app.js

--- section-8/arrays-10-map/app.js
+++ section-8/arrays-10-map/app.js
@@ -13,7 +13,7 @@ console.log(taxAdjustedPrices); */
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
 
-const taxAdjustedPrices = prices.map((price, idx, prices) => { // unlike forEach(), map() has to return something. map() takes an array, runs a fuction on each element in array then returns it.It doesn not change pointed array but makes a new one
+const taxAdjustedPrices = prices.map((price, idx, prices) => { // unlike forEach(), map() has to return something. map() takes an array, runs a call back fuction on each element in array then returns it.It doesn not change pointed array but makes a new one
   const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) }; //arguments => elemnt in array, index number, array number same as forEach()
   return priceObj;
 });
