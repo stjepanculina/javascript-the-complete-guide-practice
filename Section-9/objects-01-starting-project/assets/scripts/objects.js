c9a4334a45124783079cf2e473d443553512a887
Stjepan Čulina <stjepan.culina93@gmail.com>
2023-10-30 21:55:21 +0100

    9-215 object created&called

---
+++ Section-9/objects-01-starting-project/assets/scripts/objects.js
@@ -0,0 +1,11 @@
+const person = {
+    name : "Max",
+    age : 30,
+    hobbies : ["Sports","Cooking"],
+    greet: function() {
+        alert("Hi there!");
+    }
+
+}
+
+console.log(person.greet());
