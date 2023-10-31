877e591ec7604e97fd6b58af6d317072ed6a84d1
stjepanculina <stjepan.culina93@gmail.com>
2023-10-31 17:57:29 +0100

    215-objects-recap

---
+++ section-9/objects-01-starting-project/assets/scripts/objects.js
@@ -0,0 +1,10 @@
+const person = {
+    name : "Stefan",
+    age : 30,
+    hobbies : ["hockey, basketball"],
+    greet : function() {
+        console.log("Hi there");
+    }
+};
+
+console.log(person.greet());
