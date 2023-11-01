f57224873c4d7e2155900c6933776a14c13d7084
stjepanculina <stjepan.culina93@gmail.com>
2023-11-01 22:38:26 +0100

    220-demo app shorthand property syntax

---
+++ section-9/demo-app&shorthand-property-syntax/assets/scripts/objects.js
@@ -0,0 +1,27 @@
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
+
+const movies = [];
+
+const addMovieHandler = () => {
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
+
+  if (title === "" || extraName === "" || extraValue === "") {
+    return;
+  }
+
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue,
+    },
+    id: Math.random()
+  };
+  movies.push(newMovie);
+  console.log(newMovie);
+};
+
+addMovieBtn.addEventListener("click", addMovieHandler);
+//searchBtn.addEventListener(click);
