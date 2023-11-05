56db838fda6f3ea48b5495e1115880cad3cc8015
stjepanculina <stjepan.culina93@gmail.com>
2023-11-05 18:23:01 +0100

    trying to solve bug

--- section-9/objects-09-search-functionality/assets/scripts/objects.js
+++ section-9/objects-09-search-functionality/assets/scripts/objects.js
@@ -1,10 +1,10 @@
 const addMovieBtn = document.getElementById("add-movie-btn");
 const searchBtn = document.getElementById("search-btn");
 
-
 const movies = [];
 
-const renderMovieHandler = (filter = "") => {
+const renderMovies = (filter = "") => {
+  // we use default argument so searchFilterHandler can pass argument to renderMovie. if searchFilterHandler is not used empty string is passed to rendermovie function
   const movieList = document.getElementById("movie-list");
   if (movies.length === 0) {
     movieList.classList.remove("visible");
@@ -13,15 +13,16 @@ const renderMovieHandler = (filter = "") => {
     movieList.classList.add("visible");
   }
 
-  const filteredMovies = !filter
-    ? movies
-    : movies.info.title.filter((movies) => {
-        movies.includes(filter);
+  const filteredMovies = !filter // if filter is not truthy (fitler is empty string) then movies is passed to variable filteredMovies. if filter truthy then title from object newMovie in array movies is passed to variable filteredMovies
+    ? movies 
+    : movies.filter(movie => {
+        movie.info.title.includes(filter);
       });
 
   movieList.innerHTML = "";
 
-  filteredMovies.forEach((movie) => {
+  filteredMovies.forEach( movie => {
+    // depending on filteredMovies value
     const movieEl = document.createElement("li");
     let text = movie.info.title + " - "; // chaining method
     for (const key in movie.info) {
@@ -31,6 +32,7 @@ const renderMovieHandler = (filter = "") => {
         text = text + `${key} : ${movie.info[key]}`; // outputting dynamic properties
       }
     }
+    console.log(filteredMovies);
     movieEl.textContent = text;
     movieList.appendChild(movieEl);
   });
@@ -53,12 +55,14 @@ const addMovieHandler = () => {
     id: Math.random().toString(),
   };
   movies.push(newMovie);
-  renderMovieHandler();
+  renderMovies();
   console.log(newMovie);
 };
 
 const searchFilterHandler = () => {
-  const filterTitle = document.getElementById("filter-title").value;
+  const filterTerm = document.getElementById("filter-title").value;
+  console.log(filterTerm)
+  renderMovies(filterTerm);
 };
 
 addMovieBtn.addEventListener("click", addMovieHandler);
