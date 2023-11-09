5e1f14b84cc320681b0090ecff826f4ef7e168da
stjepanculina <stjepan.culina93@gmail.com>
2023-11-09 21:46:07 +0100

    231-making "this" keyword with bind() method

--- section-9/objects-10-destructuring/assets/scripts/objects.js
+++ section-9/objects-10-destructuring/assets/scripts/objects.js
@@ -17,26 +17,32 @@ const renderMovies = (filter = "") => {
   const filteredMovies = !filter
     ? movies
     : movies.filter((movie) => movie.info.title.includes(filter));
-  console.log(filteredMovies);
+
+  filteredMovies.console.log(filteredMovies);
 
   filteredMovies.forEach((movie) => {
+    // forEach elemnt (movie) in global movies array, perform designated code
     const movieEl = document.createElement("li");
     if ("info" in movie) {
+      // checks if info keyword exist in movie object
       // checks for property existance
       console.log(true);
     } else {
       console.log(false);
     }
     if (movie.info === undefined) {
+      // checks if info keyword exist in movie object
       // also checks for property existance
       console.log(true);
     } else {
       console.log(false);
     }
-    const { info, ...otherProp } = movie; // object destructuring.right side of = is object we want to destruct,left side of = is key names we need(info). (...otherProp) => using rest operator to add other property besides info
-    console.log(otherProp);
-   //  const { title: movieTitle } = info;  destructuring info object  and changing key name "title" to "movieTitle"
-    let text = movie.getFormatedTitle() + ' - '; // thanks to this keyword we can call a function ("getFormatedTitle") inside an object 
+    // const { info, ...otherProp } = movie; // object destructuring.right side of = is object we want to destruct,left side of = is key names we need(info). (...otherProp) => using rest operator to add other property besides info
+    //  const { title: movieTitle } = info;  destructuring info object  and changing key name "title" to "movieTitle"
+    // let text = movie.getFormatedTitle() + ' - '; // thanks to this keyword we can call a function ("getFormatedTitle") inside an object
+    // another method for this keyword
+    const { getFormatedTitle } = movie; // destructured function from object movie
+    getFormatedTitle = getFormatedTitle.bind(movie); // binds this keyword to movie object
     for (const key in info) {
       if (key !== "title") {
         text = text + `${key}: ${info[key]}`;
@@ -51,6 +57,7 @@ const addMovieHandler = () => {
   const title = document.getElementById("title").value;
   const extraName = document.getElementById("extra-name").value;
   const extraValue = document.getElementById("extra-value").value;
+  console.log(extraName);
 
   if (
     title.trim() === "" ||
@@ -65,15 +72,14 @@ const addMovieHandler = () => {
       title,
       [extraName]: extraValue,
     },
-    id: Math.random(),
-/*     getFormatedTitle: function () { longer syntax
-      return this.info.title.toUpperCase(); // this keyword is refering to this object ("newMovie") 
+    id: Math.random().toString(),
+    /*     getFormatedTitle: function () { longer syntax
+      return this.info.title.toUpperCase(); 
     }, */
     getFormatedTitle() {
-      return this.info.title.toUpperCase();
-    }
+      return this.info.title.toUpperCase(); // this keyword is refering to this object ("newMovie")
+    },
   };
-
   movies.push(newMovie);
   renderMovies();
 };
