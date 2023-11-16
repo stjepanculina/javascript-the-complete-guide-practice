6f2670a6124c18b5ceb876a7f9850fb18308dfc5
stjepanculina <stjepan.culina93@gmail.com>
2023-11-16 19:45:54 +0100

    trying to implement filter functionality

--- practice/section-9/search and filter app/assets/scripts/objects.js
+++ practice/section-9/search and filter app/assets/scripts/objects.js
@@ -1,11 +1,10 @@
 const addMovieBtn = document.getElementById("add-movie-btn");
 
-const addfilterBtn = document.getElementById("filter-title");
 const searchBtn = document.getElementById("search-btn");
 
 let movies = [];
 
-const someFnc = () => {
+const getAndPushData = () => {
   const titleInput = document.getElementById("title").value;
   const extraName = document.getElementById("extra-name").value;
   const extraValue = document.getElementById("extra-value").value;
@@ -15,22 +14,39 @@ const someFnc = () => {
       "Favorite Movie,Extra Name and Value input should not be empty!"
     );
   } else {
-    const movieList = document.getElementById('movie-list');
-    movieList.classList.add('visible');
-    const li = document.createElement('li');
-    movieList.append(li);
-    li.textContent = 'some text'
-    // listMovieEl.innerHTML = 'some text'
+    if (!titleInput || !extraName || !extraValue) {
+      titleInput.trim();
+      extraName.trim();
+      extraValue.trim();
+    }
+    const movieData = {
+      title: titleInput.toUpperCase(),
+      name: extraName,
+      value: extraValue,
+      id: Math.random(),
+    };
+    movies.push(movieData);
+    console.log(movieData, movies);
+    //renderMovies();
+    renderMovies(movieData);
   }
-
-  const movieData = {
-    title: titleInput,
-    name: extraName,
-    value: extraValue,
-  };
-  movies.push(movieData);
-  console.log(movieData, movies);
 };
 
-addMovieBtn.addEventListener("click", someFnc);
-// addFilterBtn.addEventListener('onClick', someFnc);
+const someFnc = () => {
+  const addfilterBtn = document.getElementById("filter-title").value;
+  // const inputText = addfilterBtn.toString();
+  const filteredMovies = movies.filter((movie) => {
+    movie.titleInput.includes(addfilterBtn);
+  });
+  console.log(filteredMovies);
+};
+const renderMovies = (movieInfo = "") => {
+  const movieList = document.getElementById("movie-list");
+  movieList.classList.add("visible");
+  const li = document.createElement("li");
+  movieList.append(li);
+  li.innerHTML = `${movieInfo.title} - ${movieInfo.name} ${movieInfo.value}`;
+};
+
+addMovieBtn.addEventListener("click", getAndPushData);
+searchBtn.addEventListener("click", someFnc);
