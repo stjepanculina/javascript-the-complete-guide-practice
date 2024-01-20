02987b3eb9751d542b5d0fcfce42509b868a56b7
stjepanculina <stjepan.culina93@gmail.com>
2024-01-20 16:38:06 +0100

    replacing objects literal with classes,adding constructor

--- practice/section-10/OOB-constructor-methods/javascript.js
+++ practice/section-10/OOB-constructor-methods/javascript.js
@@ -1,34 +1,51 @@
-const products = [
-  {
-    name: "Steam Deck LCD",
-    price: "$399",
-    description:
-      "Steam Deck LCD Version Featuring: 64 gb eMMC of Memory, 16 GB LPDDR5 RAM",
-    image: `https://media.gamestop.com/i/gamestop/20003488-1?$pdp2x$`,
-  },
-  {
-    name: "Steam Controller",
-    price: "$100",
-    description: "Steam Controller designed by Valve made for you!",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Steam_Controller_B.jpg/1024px-Steam_Controller_B.jpg",
-  },
-];
+class Product {
+  title;
+  price;
+  description;
+  image;
 
-const render = () => {
-  const appEl = document.getElementById("app");
-  const ulEl = document.createElement("ul");
-  for (const prod of products) {
-    const liEl = document.createElement("li");
-    liEl.className = "list-el";
-    liEl.innerHTML = `
-   
-   <img src="${prod.image}" width="30%" alt="${prod.name} " > 
-   
-    `;
-    ulEl.append(liEl);
+  constructor(title, price, description, image) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.image = image;
   }
-  appEl.append(ulEl);
+}
+
+const productsList = {
+  products: [
+    new Product(
+      "Steam Deck LCD",
+      "$399",
+      "Steam Deck LCD Version Featuring: 64 gb eMMC of Memory, 16 GB LPDDR5 RAM",
+      `https://media.gamestop.com/i/gamestop/20003488-1?$pdp2x$`
+    ),
+    new Product(
+      "Steam Controller",
+      "$100",
+      "Steam Controller! Designed by Valve made for you!",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Steam_Controller_B.jpg/1024px-Steam_Controller_B.jpg"
+    ),
+  ],
+  render() {
+    const appEl = document.getElementById("app");
+    const ulEl = document.createElement("ul");
+    for (const prod of this.products) {
+      /* this keyword is an object(productsList) */
+      const liEl = document.createElement("li");
+      liEl.className = "list-el";
+      liEl.innerHTML = `
+     <img src="${prod.image}" width="30%" alt="${prod.title} " > 
+     <div> 
+     <h3> ${prod.title} </h3>
+     <h4> Price: ${prod.price} </h4>
+     <p>Description: ${prod.description}</p>
+     </div>
+      `;
+      ulEl.append(liEl);
+    }
+    appEl.append(ulEl);
+  },
 };
 
-render();
+productsList.render();
